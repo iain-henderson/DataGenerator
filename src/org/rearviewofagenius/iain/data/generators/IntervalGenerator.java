488cc3c8ddddfe1a802f5c94c485e2bb71e9784e
IainJamesHenderson <i.henderson@terrawi.com>
2015-09-01 00:43:46 -0400

    Added a generator for intervals

---
+++ src/org/rearviewofagenius/iain/data/generators/IntervalGenerator.java
@@ -0,0 +1,30 @@
+package org.rearviewofagenius.iain.data.generators;
+
+public class IntervalGenerator extends Generator {
+
+	  double days;
+	  double hours;
+	  double minutes;
+	  double seconds;
+	  
+	  public IntervalGenerator(Integer rows)
+	  {
+	    super(Integer.valueOf(queueDepth), rows);
+	  }
+	  
+	  public String generate()
+	  {
+	    days = (nextInt(730) + nextGaussian());
+	    hours = ((days - (int)days) * 24.0D);
+	    minutes = ((hours - (int)hours) * 60.0D);
+	    seconds = ((minutes - (int)minutes) * 60.0D);
+	    
+	    return String.format("%d %d:%d:%d", (int)days, (int)hours, (int)minutes, (int)seconds);
+	  }
+	  
+	  public String toString()
+	  {
+	    return "Interval Generator";
+	  }
+
+}
